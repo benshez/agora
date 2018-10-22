8a5a51c3ea2d5aa9107666a418af78d31f562b67
Ben van Heerden <benshez@gmail.com>
2018-10-23 07:04:08 +1000

    Added TNS about providers.

--- apps/angular/src/app/views/about/module.tns.ts
+++ apps/angular/src/app/views/about/module.tns.ts
@@ -1,9 +1,10 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { COMPONENT_MODULES, COMPONENT_DECLARATIONS } from '@views/about/common';
+import { COMPONENT_MODULES, COMPONENT_DECLARATIONS, COMPONENT_PROVIDERS } from '@views/about/common';
 
 @NgModule({
     imports: [...COMPONENT_MODULES],
     declarations: [...COMPONENT_DECLARATIONS],
+    providers: [...COMPONENT_PROVIDERS],
     schemas: [NO_ERRORS_SCHEMA]
 })
 export class AboutModule { }
