27eeb5789398ba8640ba9d0782c531adeff2758b
Ben van Heerden <benshez@gmail.com>
2018-11-14 06:54:39 +1000

    Fix webpack build issues.

--- apps/angular/src/app/app.module.tns.ts
+++ apps/angular/src/app/app.module.tns.ts
@@ -9,9 +9,9 @@ import { AppComponent } from './app.component';
 
 import { TNS_COMPONENT_MODULES } from './app.common.tns';
 
-import { Config } from '@common/utils/Config';
+// import { Config } from '@common/utils/Config';
 
-Config.PLATFORM_TARGET = Config.PLATFORMS.MOBILE_NATIVE;
+// Config.PLATFORM_TARGET = Config.PLATFORMS.MOBILE_NATIVE;
 
 @NgModule({
     declarations: [AppComponent],
